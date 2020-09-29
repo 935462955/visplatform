c82c00a0e77821042146fa6a4deae1bbbf5b3311
935462955 <935462955@qq.com>
2020-09-29 10:35:04 +0800

    配置文件添加远程数据库地址

--- visplatform/visplatform/settings.py
+++ visplatform/visplatform/settings.py
@@ -5,9 +5,14 @@ from visplatform import app
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
 MONGODB_SETTINGS ={
     'db': 'vis',
-    'host': '127.0.0.1',
-    'port': 27017
+    'host': '103.219.39.150',
+    'port': 20055
 }
+# MONGODB_SETTINGS ={
+#     'db': 'vis',
+#     'host': '127.0.0.1',
+#     'port': 27017
+# }
 CSRF_ENABLED = True
 COURSE_UPLOAD_FOLDER = 'visplatform/data'
 PROJECT_UPLOAD_FOLDER = 'visplatform/js/test4project'
